bbec73f3cac061b8125dee5adfcaeb6f78879d9a
root <root@ip-172-31-95-96.ec2.internal>
2025-12-24 15:50:07 +0000

    first commit

---
+++ student.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        double sum = num1 + num2;
+        double diff = num1 - num2;
+
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + diff);
+
+        scanner.close();
+    }
+}
