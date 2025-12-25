1c8510c03e4fdd0e79d5c4728cf98a6b12f484f4
rajeshasodu6666-byte <rajeshasodu6666@gmail.com>
2025-12-25 16:22:12 +0530

    Update student.java

--- student.java
+++ student.java
@@ -1,21 +1,18 @@
-import java.util.Scanner;
-
-public class Calculator {
-
+public class Student {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        double num1 = scanner.nextDouble();
-
-        System.out.print("Enter second number: ");
-        double num2 = scanner.nextDouble();
-
-        double sum = num1 + num2;
-        double diff = num1 - num2;
-
-        System.out.println("Addition: " + sum);
-       
-        scanner.close();
+        int rollno = 1596;
+        String sname = "KRISHNA";
+        String course = "PYTHON";
+        System.out.println("LEARN HERE AND LEAD ANYWHERE!!!");
+        System.out.println("My Name is: " + sname);
+        System.out.println("My Course is: " + course);
+        System.out.println("My Roll Number is: " + rollno);
+        System.out.println("=======================");
+        System.out.println("JENKINS-WEBHHOK-TRIIGER");
+         System.out.println("GOOD MORNING");
+         System.out.println("UPDATED");
+        System.out.println("=======================");
     }
 }
+
+
